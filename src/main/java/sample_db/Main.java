cf0c42c8fec6405151b078e47adac91e604773f2
Snopoff <snopoffp@gmail.com>
2018-10-14 20:20:27 +0300

    added some comments for comprehension

--- src/main/java/sample_db/Main.java
+++ src/main/java/sample_db/Main.java
@@ -4,11 +4,20 @@ public class Main {
     public static void main(String[] args) {
         Integer[] arr = {3,1,4};
         CustomList<Integer> list = new CustomList<>(arr);
-        list.print();
+
+        list.print();  //[3, 1, 4, ]
+
         list.add(6);
-        list.print();
+        list.print();  //[3, 1, 4, 6, ]
+
         Integer intToRemove = 3;
         list.remove(intToRemove);
-        list.print();
+        list.print();  //[1, 4, 6, ]
+
+        list.remove(3);
+        list.print();  //[3, 1, 4, ]
+
+        list.remove(1);
+        list.print();  //[3, 4, 6, ]
     }
 }
